5620ec47a9ccbecf7dabd2fc3ae4178609b3f5fc
Leonardo Barbosa Silva <leonardobarbosassilva@gmail.com>
2023-07-04 08:21:35 -0300

    express static

--- Documents/PROGRAMMING/PROJECTS/APPLICATION/ToDreams/backend/src/server.js
+++ Documents/PROGRAMMING/PROJECTS/APPLICATION/ToDreams/backend/src/server.js
@@ -21,6 +21,6 @@ const path = require('path');
     app.use(helmet())
     app.use(express.json({ limit: '30mb' }))
     app.use(express.urlencoded({ limit: '30mb', extended: true }))
-    app.use(express.static(path.join(__dirname, '../../frontend')))
+    app.use(express.static(path.join(__dirname, '../../frontend/build', 'index.html')))
     app.use('/todreams', routes)
 })()
