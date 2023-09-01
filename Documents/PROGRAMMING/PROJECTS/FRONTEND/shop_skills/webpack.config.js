5e900ec8de72c95cb8dcc495fb3b6e85071fc58b
Leonardo Barbosa Silva <leonardobarbosassilva@gmail.com>
2023-09-01 09:12:38 -0300

    webpack deploy

--- Documents/PROGRAMMING/PROJECTS/FRONTEND/shop_skills/webpack.config.js
+++ Documents/PROGRAMMING/PROJECTS/FRONTEND/shop_skills/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './index.js',
     output: {
-        path: path.resolve(__dirname, './src/dist'),
+        path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
     },
     module: {
