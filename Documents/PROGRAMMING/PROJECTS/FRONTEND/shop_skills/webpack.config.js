1ea681ce0536b4348afd54712b70276a4c3551fa
Leonardo Barbosa Silva <leonardobarbosassilva@gmail.com>
2023-09-01 09:20:13 -0300

    webpack build correct directory

--- Documents/PROGRAMMING/PROJECTS/FRONTEND/shop_skills/webpack.config.js
+++ Documents/PROGRAMMING/PROJECTS/FRONTEND/shop_skills/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './index.js',
     output: {
-        path: path.resolve(__dirname, 'public'),
+        path: path.resolve(__dirname, 'public', 'dist'),
         filename: 'bundle.js'
     },
     module: {
