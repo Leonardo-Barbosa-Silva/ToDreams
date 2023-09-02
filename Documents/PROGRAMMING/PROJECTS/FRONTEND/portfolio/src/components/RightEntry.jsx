e12de7edb85ac9a25232faa63839c63dd02c782f
Leonardo Barbosa Silva <leonardobarbosassilva@gmail.com>
2023-09-02 19:49:21 -0300

    middle project

---
+++ Documents/PROGRAMMING/PROJECTS/FRONTEND/portfolio/src/components/RightEntry.jsx
@@ -0,0 +1,16 @@
+import ShpereDistort from "./SphereDistort";
+
+
+const RightEntry = () => {
+
+    return (
+        <div className='right'>
+
+            <ShpereDistort />
+            
+            <img src="./src/img/astronaut.png" alt="animation astronaut moving to top and down" />
+        </div>
+    )
+}
+
+export default RightEntry;
